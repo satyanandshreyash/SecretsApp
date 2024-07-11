3fb5a8356335e674329d07d4e633a46652ef83b7
satyanandshreyash <satyanandshreyash@gmail.com>
2024-07-12 01:35:02 +0530

    Change level of security to hashing using md5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -24,9 +24,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-var secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
-
 const User = mongoose.model('User', userSchema);
 
 app.get("/", function(req, res){
@@ -44,7 +41,7 @@ app.get('/login', function(req, res){
 app.post('/register', function(req, res){
     const newUser = new User({
         userName: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save();
     res.render('login');
@@ -52,7 +49,7 @@ app.post('/register', function(req, res){
 
 app.post('/login', function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({userName: username}).then((foundUser)=>{
         if(foundUser.password === password){
             res.render('secrets');
